6e6178140d8510e1183a0678e1772ffad3ab1139
VIanor <37715657+VIanor@users.noreply.github.com>
2019-03-25 15:19:16 +0300

    Pokedex-build

---
+++ precache-manifest.c5bf626210d251b363c6b8ba8f23fb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ddefb2673fc621de3d2",
+    "url": "/Pokedex-app/static/js/runtime~main.ebf6e4c7.js"
+  },
+  {
+    "revision": "c3dfc55028dde3075bd7",
+    "url": "/Pokedex-app/static/js/main.fb856b2f.chunk.js"
+  },
+  {
+    "revision": "fc0cdf0ea0519ed6412c",
+    "url": "/Pokedex-app/static/js/2.c0992fc7.chunk.js"
+  },
+  {
+    "revision": "c3dfc55028dde3075bd7",
+    "url": "/Pokedex-app/static/css/main.53c2e240.chunk.css"
+  },
+  {
+    "revision": "4d9321d0d721862e300ecbdfc7491c67",
+    "url": "/Pokedex-app/index.html"
+  }
+];
